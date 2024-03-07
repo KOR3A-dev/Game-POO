1efc0361a85968b878366c0ae63769b247ffb2c5
alejandro valencia <ingeniero.desarrollo9@comersantander.com>
2024-03-07 08:49:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# game_poo
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/game_poo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.game_poo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
